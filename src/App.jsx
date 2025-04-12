ab0a9d7145d259199d447bbe0f051ab3745823ae
ayush00git <ayush.2007.iit@gmail.com>
2025-04-12 18:55:04 +0530

    Home page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Homepage from "./componnets/HomePage";
 import ChatModal from "./componnets/ChatModal";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<ChatModal />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Homepage />} />
       </Routes>
     </Router>
